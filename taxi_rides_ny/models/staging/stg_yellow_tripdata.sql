562ec22b0e9c57ed56b648db976c2ae07b242a85
Tumi <tumi@chromazone.tv>
2026-02-16 23:56:13 +0100

    dbt_models

---
+++ taxi_rides_ny/models/staging/stg_yellow_tripdata.sql
@@ -0,0 +1,39 @@
+with tripdata as (
+  select *
+  from {{ source('staging','yellow_tripdata') }}
+  where vendorid is not null 
+),
+
+renamed as (
+  select
+      -- identifiers
+      cast(vendorid as integer) as vendor_id,
+      cast(ratecodeid as integer) as rate_code_id,
+      cast(pulocationid as integer) as pickup_locationid,
+      cast(dolocationid as integer) as dropoff_locationid,
+      
+      -- timestamps
+      cast(tpep_pickup_datetime as timestamp) as pickup_datetime,
+      cast(tpep_dropoff_datetime as timestamp) as dropoff_datetime,
+      
+      -- trip info
+      store_and_fwd_flag,
+      cast(passenger_count as integer) as passenger_count,
+      cast(trip_distance as numeric) as trip_distance,
+      1 as trip_type, --yellow taxi can only be street-hail, so we can set this to 1 for all records
+      
+      -- payment info
+      cast(fare_amount as numeric) as fare_amount,
+      cast(extra as numeric) as extra,
+      cast(mta_tax as numeric) as mta_tax,
+      cast(tip_amount as numeric) as tip_amount,
+      cast(tolls_amount as numeric) as tolls_amount,
+      cast(improvement_surcharge as numeric) as improvement_surcharge,
+      cast(total_amount as numeric) as total_amount,
+      cast(payment_type as integer) as payment_type,
+      0 as ehail_fee -- yellow taxi doesn't have ehail_fee column, so we can set this to 0 for all records
+
+  from tripdata
+)
+
+select * from renamed
